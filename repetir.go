9aceed36e0fdba945f0d0c103168c0af5b7ea09d
Ricardo César de Oliveira <rioliveira@bionexo.com>
2020-02-09 02:07:41 -0300

    Added benchmark method

--- repetir.go
+++ repetir.go
@@ -1,8 +1,10 @@
 package iteracao
 
+const quantidadeRepeticoes = 5
+
 func Repetir(caractere string) string {
 	var repeticoes string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < quantidadeRepeticoes; i++ {
 		repeticoes += caractere
 	}
 	return repeticoes
--- repetir_test.go
+++ repetir_test.go
@@ -10,3 +10,9 @@ func TestRepetir(t *testing.T) {
 		t.Errorf("esperado '%s' mas obteve '%s'", esperado, repeticoes)
 	}
 }
+
+func BenchmarkRepetir(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repetir("a")
+	}
+}
